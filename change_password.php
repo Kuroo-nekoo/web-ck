452bd0aee402a69f72c2b80f7983e151251d461f
Kuroo-nekoo <phamtrihung5363@gmail.com>
2022-05-24 22:29:57 +0700

    fix: change redirect url

--- change_password.php
+++ change_password.php
@@ -1,5 +1,6 @@
 <?php
 require_once './db.php';
+
 session_start();
 if (isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
@@ -14,7 +15,7 @@ if (isset($_POST['new_password']) && isset($_POST['confirm_new_password']) && is
     } else {
         $data = change_password($old_password, $new_password, $user_id);
         if ($data['code'] === 0) {
-            header('Location: login.php');
+            header('Location: user.php');
         } else if ($data['code'] === 1) {
             $error_message = $data['error'];
         }
