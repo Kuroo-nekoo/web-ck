5002f60b4b360d1553ad2d67cd8284862066d298
HinFan1402 <106007494+HinFan1402@users.noreply.github.com>
2022-05-28 07:16:02 +0700

    cái git t bị lỗi merge , nên t up lại file db

--- db.sql
+++ db.sql
@@ -35,10 +35,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `account` (
   `USER_ID` int(11) NOT NULL,
-  `PHONE_NUMBER` varchar(20) NOT NULL,
+  `PHONE_NUMBER` varchar(20) NOT NULL UNIQUE,
   `EMAIL` varchar(50) NOT NULL,
   `FULL_NAME` varchar(50) NOT NULL,
-  `DATE_OF_BIRTH` date NOT NULL,
+  `DATE_OF_BIRTH` date NOT NULL, 
   `ADDRESS` varchar(50) NOT NULL,
   `USERNAME` varchar(10) DEFAULT NULL,
   `PASSWORD` varchar(10) DEFAULT NULL,
@@ -49,11 +49,10 @@ CREATE TABLE `account` (
   `IS_LOCKED` bit(1) DEFAULT b'0',
   `DATE_LOCKED` datetime DEFAULT NULL,
   `DATE_CREATED` datetime NOT NULL,
-  `BALANCE` int(11) NOT NULL
+  `BALANCE` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `account`
+
 --
 
 -- test user
@@ -71,17 +70,28 @@ INSERT INTO `account` (`USER_ID`, `PHONE_NUMBER`, `EMAIL`, `FULL_NAME`, `DATE_OF
 ALTER TABLE `account`
   ADD PRIMARY KEY (`USER_ID`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
   MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE `history` (
+        `ID` int(11) not null PRIMARY KEY,
+        `USER_ID` int(11) not null,
+        `RECEIVER_USER_ID` int(11),
+        `RECEIVER_PHONE` varchar(20),
+        `AMOUNT`  float ,
+        `TIME` varchar(20) NOT NULL
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+ALTER TABLE `history`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+alter table `history`
+add CONSTRAINT `fk_history` FOREIGN key (`USER_ID`) REFERENCES `account`(`USER_ID`)
